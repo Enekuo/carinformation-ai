b1631c890c0020837f997938a5a5ca0b914e9a4f
Eneko Aramberri <aramberrieneko@gmail.com>
2025-09-28 20:06:25 +0200

    5

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { LanguageProvider } from './lib/i18n.js';
+import { LanguageProvider } from './lib/i18n.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
