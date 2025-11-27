0233cb8d57cae08746179e2bdd9641f4f9e81d15
Eneko Aramberri <aramberrieneko@gmail.com>
2025-11-27 19:44:28 +0100

    94

--- src/components/ProAccount/ProHelp.jsx
+++ src/components/ProAccount/ProHelp.jsx
@@ -196,8 +196,8 @@ export default function ProHelp() {
                   </p>
                 </div>
 
-                {/* COLITA DEL BOCADILLO, POSICIÓN COMO EN LA CAPTURA */}
-                <div className="absolute -left-3 top-6">
+                {/* COLITA DEL BOCADILLO CENTRADA EN ALTURA */}
+                <div className="absolute -left-3 top-1/2 -translate-y-1/2">
                   {/* borde */}
                   <div className="w-0 h-0 border-y-[12px] border-y-transparent border-r-[16px] border-r-slate-200"></div>
                   {/* relleno blanco */}
