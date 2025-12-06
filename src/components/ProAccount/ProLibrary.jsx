01c11e4fdf93252335c4371deee09a8157b012f5
Eneko Aramberri <aramberrieneko@gmail.com>
2025-12-06 18:54:02 +0100

    73

--- src/components/ProAccount/ProLibrary.jsx
+++ src/components/ProAccount/ProLibrary.jsx
@@ -286,10 +286,10 @@ export default function ProLibrary() {
                   >
                     <div className="h-full w-full px-5 pt-8 pb-6 flex flex-col">
                       <img
-                        src="/LibraryCorrector.png"
+                        src="/Library3.png"
                         alt=""
-                        width={40}
-                        height={40}
+                        width={60}
+                        height={60}
                         className="block select-none"
                       />
                       <h3
