ffb71cb8c6b712674cb61d79a3034ad71c71ea72
Eneko Aramberri <aramberrieneko@gmail.com>
2025-12-06 17:59:46 +0100

    66

--- src/components/ProAccount/ProLibrary.jsx
+++ src/components/ProAccount/ProLibrary.jsx
@@ -289,8 +289,8 @@ export default function ProLibrary() {
                       <img
                         src="/Library3.png"
                         alt="Zuzenketa gramatikala"
-                        width={60}
-                        height={60}
+                        width={80}
+                        height={80}
                         className="block select-none"
                       />
                       <h3 className="mt-6 text-[18px] leading-[24px] pr-4 font-semibold text-slate-900">
