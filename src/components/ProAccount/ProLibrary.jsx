7ceabef00eab9db0154d0205542a603f1f89bc7c
Eneko Aramberri <aramberrieneko@gmail.com>
2025-12-20 12:55:18 +0100

    33

--- src/components/ProAccount/ProLibrary.jsx
+++ src/components/ProAccount/ProLibrary.jsx
@@ -511,7 +511,7 @@ export default function ProLibrary() {
                           className="block select-none -mt-2 -mb-4 -ml-6"
                         />
                         <h3
-                          className="mt-8 text-[18px] leading-[24px] pr-4"
+                          className="mt-6 text-[18px] leading-[24px] pr-4"
                           style={{
                             display: "-webkit-box",
                             WebkitLineClamp: 2,
